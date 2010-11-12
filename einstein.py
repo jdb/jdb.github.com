a59efa7867c4a192253b01444e8dd964428cef40
Jean Daniel Browne <jeandaniel.browne@gmail.com>
2010-11-12 09:46:15 +0100

    Added the assertions, needs the objects model, and assertion database

---
+++ einstein.py
@@ -0,0 +1,16 @@
+
+
+brit.house.color==red
+swede.pet==dog
+dan.drink==tea
+houses[green].right()==houses[white]
+houses[green].owner.drink==coffee
+owner.cigarets[PallMall].pet==birds
+houses[yellow].owner.cigarets==Dunhill
+houses.center().owner.drink==milk
+norwegian.house.number==1
+pets[cat].house in owners[blend].house.next()
+cigarets[Dunhill].house in pets[horses].house.next()
+cigarets[BlueMaster].drink==beer
+norwegian.house in houses[blue].next()
+cigarets[blend].house in drinks[water].house.next()
